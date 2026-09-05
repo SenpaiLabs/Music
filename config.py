87631ad6ac24c5e6ecb7e897dfe489e41b1a8389
SenpaiLabs <piyush000yogi@gmail.com>
2026-09-05 17:45:46 +0530

    Fix COOKIES_URL environment variable handling

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class Config:
         self.LANG_CODE = getenv("LANG_CODE", "en")
 
         self.COOKIES_URL = [
-            url for url in getenv("COOKIES_URL", "https://batbin.me/deejay").split(" ")
+            url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
         self.PING_IMG = getenv("PING_IMG", "https://ibb.co/xSdP4GkB")
